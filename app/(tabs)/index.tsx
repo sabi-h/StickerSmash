d4abd4c60c57aba9be5e1108e4e3a340476945e2
Sabih Hasan <17186450+sabih-h@users.noreply.github.com>
2025-02-20 22:05:08 +0000

    finish home screen ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
 import { View, StyleSheet } from 'react-native';
-import { Image } from 'expo-image';
 
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
 
-const PlaceholderImage = require('@/assets/images/background-image.png');
-
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme='primary' />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -23,10 +27,10 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
